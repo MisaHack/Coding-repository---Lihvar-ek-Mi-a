94c1a29ea31398691c12b3b4fa3787a052426817
Misa <lihvarcekmisa@yahoo.com>
2018-04-27 18:20:54 +0200

    Loops + Function

---
+++ c_programming/ForDemo10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+   for(int counter=1; counter < 11; counter++)
+      printf("%i green bottles, hanging on the wall\n", counter);
+
+   return 0;
+}
---
+++ c_programming/FunctDemo10.c
@@ -0,0 +1,17 @@
+/* My first function! :)*/
+
+#include <stdio.h>
+
+int larger(int a,int b){
+   if(a > b)
+      return a;
+   return b;
+}
+
+int main(){
+   int greatest = larger(100,1000);
+
+   printf("%i is the greatest!", greatest);
+
+   return 0;
+}
