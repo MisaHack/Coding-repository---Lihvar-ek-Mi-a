e91f6633b5750be8a3dc0b2040e3b5c26aea7661
Misa <lihvarcekmisa@yahoo.com>
2018-04-29 22:20:16 +0200

    C new

--- c_programming/FunctDemo10.c
+++ c_programming/FunctDemo10.c
@@ -11,7 +11,7 @@ int larger(int a,int b){
 int main(){
    int greatest = larger(100,1000);
 
-   printf("%i is the greatest!", greatest);
+   printf("%i is the greatest!\n", greatest);
 
    return 0;
 }
---
+++ c_programming/VoidDemo11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void no_return(){
+   puts("This function doesn't return value.");
+ }
+
+int main(){
+   no_return();
+
+   return 0;
+}
