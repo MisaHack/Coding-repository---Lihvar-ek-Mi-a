344620302544f46f29172e5571c09000de72c31b
Misa <lihvarcekmisa@yahoo.com>
2018-09-04 09:42:48 +0200

    HTML 5 - 14

---
+++ java_programming/java_new/project_demo/src/package_1/Demo_1.java
@@ -0,0 +1,17 @@
+package package_1;
+
+public class Demo_1 {
+
+	public static void main(String[] args) {
+	   String s1 = "Java";
+	   String s2 = s1;
+	   
+	   if(s1.equals(s2)){
+	      System.out.println("Strings are equal");
+	   }
+	   else {
+		  System.out.println("Strings are not equal"); 
+	   }
+	}
+	
+}
