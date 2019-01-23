9ed92514fcf9b59f396ba68d1beebd2b507ad6da
Misa <lihvarcekmisa@yahoo.com>
2019-01-23 11:34:20 +0100

    HTML 5 - 26

---
+++ Misa_JS/HTML_5/Part_6/project_26/CSS3_Demo.js
@@ -0,0 +1,9 @@
+function toNight(){
+   var nightImage = document.getElementById("nightImage");
+   nightImage.className = "solid";
+}
+
+function toDay(){
+   var nightImage = document.getElementById("nightImage");
+   nightImage.className = "transparent";
+}
